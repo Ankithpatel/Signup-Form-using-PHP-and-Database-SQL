dee0471e4d1f323690c48bcbe23ef1e6d46664cb
Deep Bag <43214783+deepbag@users.noreply.github.com>
2020-10-01 17:58:54 +0530

    Update Server.php
    I have seen your repository but in this repository, I reading difficulty and some problem fix so I can update this file.

--- server.php
+++ server.php
@@ -1,15 +1,30 @@
 <?php
-$username = " ";
-$email = " ";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "test";
 $errors = array();
 
-$db = mysqli_connect('localhost', 'root', '','test');
+$db = mysqli_connect($host, $user, $pass, $dbname);
 
 if(isset($POST['register'])){
-	$username = mysqli_real_escape_string($POST['username']);
-	$email = mysqli_real_escape_string($POST['email']);
-	$password_1 = mysqli_real_escape_string($POST['password_1']);
-	$password_2 = mysqli_real_escape_string($POST['password_2']);
+	
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password_1 = $POST['password_1'];
+	$password_2 = $POST['password_2'];
+	
+	/* to prevent sql injection mysqli_real_escape_string */
+	$username = mysqli_real_escape_string($username);
+	$email = mysqli_real_escape_string($email);
+	$password_1 = mysqli_real_escape_string($password_1);
+	$password_2 = mysqli_real_escape_string($password_2);
+	
+	/* to prevent xss attack htmlentities */
+	$username = htmlentities($username);
+	$email = htmlentities($email); 
+        $password_1 = htmlentities($password_1);
+	$password_2 = htmlentities($password_2); 
 
 	if(empty($username)){
 		array_push($errors, "Username is required");
@@ -30,7 +45,7 @@ if(isset($POST['register'])){
 	if(count($errors) == 0)
 	{
 		$password = md5($password_1);
-		$sql = "INSERT INTO users (username, email, password) Values ('$username', '$email', '$password')";
+		$sql = "INSERT INTO users (username, email, password) Values ('$username', '$email', '$password_1')";
 		mysqli_query($db, $sql);
 	}
 }
